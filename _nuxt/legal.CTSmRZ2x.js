c2d97a75f7a7b89704c005c40d6f44a0459dfe88
deployment <appdb@protonmail.com>
2024-03-07 16:51:07 +0200

    deployment

---
+++ _nuxt/legal.CTSmRZ2x.js
@@ -0,0 +1,2 @@
+import{D as e,E as r}from"./entry.jZRaifBp.js";function s(t={}){return e("get_agreements",{params:t})}function g(t){return r("submit_copyright_claim",{data:t})}function a(){return e("get_copyright_claims")}export{a,s as g,g as s};
+//# sourceMappingURL=legal.CTSmRZ2x.js.map
