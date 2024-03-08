20c8821db945d605251dc78ff7efaedb35ecccb6
deployer <appdb@protonmail.com>
2024-03-08 15:58:20 +0200

    deployment

--- _nuxt/legal.BUAUFnr6.js
+++
@@ -1,2 +0,0 @@
-import{D as e,E as r}from"./entry.D9qsLJN7.js";function s(t={}){return e("get_agreements",{params:t})}function g(t){return r("submit_copyright_claim",{data:t})}function a(){return e("get_copyright_claims")}export{a,s as g,g as s};
-//# sourceMappingURL=legal.BUAUFnr6.js.map
