7ad1c19365025f54a475299965d046b6415b2ef1
deployer <appdb@protonmail.com>
2024-03-09 13:18:46 +0200

    deployment

--- _nuxt/legal.o243G6Ki.js
+++
@@ -1,2 +0,0 @@
-import{D as e,E as r}from"./entry.DGdmtcfV.js";function s(t={}){return e("get_agreements",{params:t})}function g(t){return r("submit_copyright_claim",{data:t})}function a(){return e("get_copyright_claims")}export{a,s as g,g as s};
-//# sourceMappingURL=legal.o243G6Ki.js.map
