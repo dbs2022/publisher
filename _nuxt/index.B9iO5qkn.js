f106618bb5b9d7cb044f7819665244a054e50c99
deployer <appdb@protonmail.com>
2024-03-10 17:00:10 +0200

    deployment

---
+++ _nuxt/index.B9iO5qkn.js
@@ -0,0 +1,2 @@
+import{f as e,o as n,c as o}from"./entry.B_SnLlif.js";const c=e({__name:"index",setup(r){return(t,s)=>(n(),o("div",null,"Work in progress..."))}});export{c as default};
+//# sourceMappingURL=index.B9iO5qkn.js.map
